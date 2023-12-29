a702e34806f21cdcbf487ba8db1611e247a440fe
Ali Nobariasl <freeeeboy.gh@gmail.com>
2023-12-29 16:43:50 +0300

    Adding hash Function

--- db/db_user.py
+++ db/db_user.py
@@ -2,13 +2,13 @@ from sqlalchemy.orm.session import Session
 
 from router.schemas import UserBase
 from db.models import DbUser
-
+from db.hashing import Hash
 
 def create_new_user(request:UserBase, db:Session):
     user = DbUser(
         username = request.username,
         email = request.email,
-        password = request.password
+        password = Hash.bcrypt(request.password)
     )
     db.add(user)
     db.commit()
